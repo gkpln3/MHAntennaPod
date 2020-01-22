41d1cb99977d7fcd27f664ce826191d65c400305
Guy Kaplan <gkpln3@gmail.com>
2020-01-22 17:00:44 +0200

    Changed icons and app name to match the current app.

--- core/src/main/java/de/danoeh/antennapod/core/service/PlayerWidgetJobService.java
+++ core/src/main/java/de/danoeh/antennapod/core/service/PlayerWidgetJobService.java
@@ -136,7 +136,7 @@ public class PlayerWidgetJobService extends SafeJobIntentService {
                 views.setImageViewBitmap(R.id.imgvCover, icon);
             } catch (Throwable tr) {
                 Log.e(TAG, "Error loading the media icon for the widget", tr);
-                views.setImageViewResource(R.id.imgvCover, R.mipmap.ic_launcher_foreground);
+                views.setImageViewResource(R.id.imgvCover, R.drawable.ic_launcher);
             }
 
             views.setTextViewText(R.id.txtvTitle, media.getEpisodeTitle());
@@ -175,8 +175,8 @@ public class PlayerWidgetJobService extends SafeJobIntentService {
             views.setViewVisibility(R.id.txtvProgress, View.GONE);
             views.setViewVisibility(R.id.txtvTitle, View.GONE);
             views.setViewVisibility(R.id.txtNoPlaying, View.VISIBLE);
-            views.setImageViewResource(R.id.imgvCover, R.mipmap.ic_launcher_foreground);
-            views.setImageViewResource(R.id.butPlay, R.drawable.ic_av_play_white_48dp);
+            views.setImageViewResource(R.id.imgvCover, R.drawable.ic_launcher);
+            views.setImageViewResource(R.id.butPlay, R.drawable.ic_play_arrow_white_48dp);
         }
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
